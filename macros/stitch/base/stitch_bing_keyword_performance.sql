97c9b138efb48bb763166c1219eb9c09dc516327
keatmin <celsiuschaos@gmail.com>
2020-05-12 02:35:07 +1000

    Converting to bigquery compatible SQL

--- macros/stitch/base/stitch_bing_keyword_performance.sql
+++ macros/stitch/base/stitch_bing_keyword_performance.sql
@@ -85,3 +85,85 @@ parsed as (
 select * from parsed
 
 {% endmacro %}
+
+{% macro bigquery__stitch_bing_keyword_performance() %}
+
+with source as (
+
+    select * from {{var('bing_keyword_performance_report_table')}}
+    
+),
+
+renamed as (
+
+    select
+    
+        "__SDC_PRIMARY_KEY" as keyword_performance_report_id,
+
+        convert_timezone('UTC', timeperiod)::timestamp_ntz::date 
+            as campaign_date,
+            
+        accountid as account_id,
+        adgroupid as ad_group_id,
+        adgroupname as ad_group_name,
+        adid as ad_id,
+        campaignid as campaign_id,
+        campaignname as campaign_name,
+        keywordid as keyword_id,
+        
+        campaignstatus as campaign_status,
+        
+        replace(
+            concat(coalesce(destinationurl, ''), coalesce(finalurl, '')),
+            '%20', ' '
+            ) as url,
+        
+        clicks,
+        impressions,
+        spend,
+        
+        rank() over (
+            partition by timeperiod::date 
+            order by _sdc_report_datetime desc
+            ) as rank
+
+    from source
+
+),
+
+parsed as (
+
+    select
+    
+        keyword_performance_report_id,
+        campaign_date,
+        keyword_id,
+        account_id,
+        ad_group_id,
+        ad_group_name,
+        ad_id,
+        campaign_id,
+        campaign_name,
+        campaign_status,
+        
+        url,
+        {{ dbt_utils.get_url_host('url') }} as url_host,
+        concat('/', {{ dbt_utils.get_url_path('url') }}) as url_path,
+        {{ dbt_utils.get_url_parameter('url', 'utm_source') }} as utm_source,
+        {{ dbt_utils.get_url_parameter('url', 'utm_medium') }} as utm_medium,
+        {{ dbt_utils.get_url_parameter('url', 'utm_campaign') }} as utm_campaign,
+        {{ dbt_utils.get_url_parameter('url', 'utm_content') }} as utm_content,
+        {{ dbt_utils.get_url_parameter('url', 'utm_term') }} as utm_term,
+        
+        clicks,
+        impressions,
+        spend
+        
+    from renamed
+    where rank = 1
+
+)
+
+select * from parsed
+
+{% endmacro %}
